ff786579a53f6eff2426768d8919377078448ee1
keita1899 <saboten18k@gmail.com>
2023-07-23 22:17:33 +0900

    welcomeページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'welcome#index'
+
   resources :products, only: %i[index show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
