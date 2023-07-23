aa5ad08936a843d0b65daaae7ddcc1bf3f2fe62e
keita1899 <saboten18k@gmail.com>
2023-07-23 21:18:43 +0900

    productsテーブルとActiveStorageのマイグレーション作成

--- db/migrate/20230718122125_create_products.rb
+++ db/migrate/20230718122125_create_products.rb
@@ -1,11 +1,18 @@
+# frozen_string_literal: true
+
 class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
+      t.string :code1, null: false, limit: 3
+      t.string :code2, null: false, limit: 3
+      t.string :code3, null: false, limit: 3
       t.string :name, null: false
       t.text :description, null: false
-      t.string :image, null: false
-      t.integer :price, null: false
+      t.integer :min_price, null: false
+      t.integer :max_price
+      t.integer :discount_price
       t.integer :stock, null: false, default: 0
+      t.boolean :is_sale, null: false, default: false
 
       t.timestamps
     end
