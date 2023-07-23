31b9c020a20e6b0b8d7b1f42d9296b58a0ae6b0c
keita1899 <saboten18k@gmail.com>
2023-07-23 21:18:59 +0900

    ダミーデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+def shuffle_code(first_str, last_str, length)
+  (first_str..last_str).to_a.shuffle[0..length].join
+end
+
+10.times do |_n|
+  product = Product.create!(
+    code1: shuffle_code('A', 'Z', 2),
+    code2: shuffle_code('A', 'Z', 2),
+    code3: shuffle_code('0', '9', 2),
+    name: '商品名',
+    description: '商品の説明',
+    min_price: 40,
+    max_price: 100,
+    discount_price: 35,
+    stock: 1
+  )
+
+  product.image.attach(io: File.open(Rails.root.join('app/assets/images/no-image.jpeg')), filename: 'no-image.jpeg')
+end
