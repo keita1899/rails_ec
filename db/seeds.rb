f9281996cbfa33066b7a235e54c9b87a994ec84b
keita1899 <saboten18k@gmail.com>
2023-07-24 23:13:11 +0900

    ダミーデータの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,18 +12,47 @@ def shuffle_code(first_str, last_str, length)
   (first_str..last_str).to_a.shuffle[0..length].join
 end
 
-10.times do |_n|
-  product = Product.create!(
+4.times do |_n|
+  product1 = Product.create!(
     code1: shuffle_code('A', 'Z', 2),
     code2: shuffle_code('A', 'Z', 2),
     code3: shuffle_code('0', '9', 2),
     name: '商品名',
     description: '商品の説明',
     min_price: 40,
-    max_price: 100,
-    discount_price: 35,
     stock: 1
   )
 
-  product.image.attach(io: File.open(Rails.root.join('app/assets/images/no-image.jpg')), filename: 'no-image.jpg')
+  product1.image.attach(io: File.open(Rails.root.join('app/assets/images/450300.jpg')), filename: '450300.jpg')
+end
+
+3.times do |_n|
+  product2 = Product.create!(
+    code1: shuffle_code('A', 'Z', 2),
+    code2: shuffle_code('A', 'Z', 2),
+    code3: shuffle_code('0', '9', 2),
+    name: '商品名',
+    description: '商品の説明',
+    min_price: 40,
+    max_price: 80,
+    stock: 1
+  )
+
+  product2.image.attach(io: File.open(Rails.root.join('app/assets/images/450300.jpg')), filename: '450300.jpg')
+end
+
+3.times do |_n|
+  product3 = Product.create!(
+    code1: shuffle_code('A', 'Z', 2),
+    code2: shuffle_code('A', 'Z', 2),
+    code3: shuffle_code('0', '9', 2),
+    name: '商品名',
+    description: '商品の説明',
+    min_price: 20,
+    discount_price: 18,
+    stock: 1,
+    is_sale: true
+  )
+
+  product3.image.attach(io: File.open(Rails.root.join('app/assets/images/450300.jpg')), filename: '450300.jpg')
 end
