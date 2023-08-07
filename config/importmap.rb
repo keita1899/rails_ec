d2034a80fe84aee0690624c00f260f006047a879
keita1899 <saboten18k@gmail.com>
2023-08-07 20:02:18 +0900

    bootstrapを初期の状態に戻す

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,2 +1,2 @@
-pin "popper", to: "popper.js", preload: true
-pin "bootstrap", to: "bootstrap.min.js", preload: true
+pin "popper", to: 'popper.js', preload: true
+pin "bootstrap", to: 'bootstrap.min.js', preload: true
