0937955e1c8edc36015bb0d19c63f406a7b4ec58
keita1899 <saboten18k@gmail.com>
2023-08-07 20:35:09 +0900

    rubocop -A

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,2 +1,4 @@
-pin "popper", to: 'popper.js', preload: true
-pin "bootstrap", to: 'bootstrap.min.js', preload: true
+# frozen_string_literal: true
+
+pin 'popper', to: 'popper.js', preload: true
+pin 'bootstrap', to: 'bootstrap.min.js', preload: true
