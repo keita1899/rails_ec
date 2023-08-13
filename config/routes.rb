c2c8e3ed866e80b5cbc9b48002e1cdc871666352
keita1899 <saboten18k@gmail.com>
2023-08-13 16:59:59 +0900

    rails g controller Admin::Products new edit index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'products/new'
+    get 'products/edit'
+    get 'products/index'
+  end
   root 'welcome#index'
 
   resources :products, only: %i[index show]
