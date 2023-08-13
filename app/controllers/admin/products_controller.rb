cdcd479549999c2d9573fe5c19f87f218a961bd3
keita1899 <saboten18k@gmail.com>
2023-08-13 19:41:25 +0900

    basic認証を実装

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,10 +1,21 @@
 class Admin::ProductsController < ApplicationController
+
+  before_action :basic_auth
+
+  def index
+  end
+
   def new
   end
 
   def edit
   end
 
-  def index
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
