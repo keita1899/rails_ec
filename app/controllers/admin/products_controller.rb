a5873e675e4fd4e57a2b08b8de071d4cb75bb8e3
keita1899 <saboten18k@gmail.com>
2023-08-18 22:42:53 +0900

    商品作成機能実装

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -5,15 +5,28 @@ module Admin
     layout 'admin'
 
     before_action :basic_auth
+    before_action :set_product, only: %i[edit update destroy]
 
     def index
       @products = Product.all.page(params[:page]).per(10).recent
     end
 
     def new
+      @product = Product.new
     end
 
     def create
+      @product = Product.new(product_params)
+
+      @product.code1 = @product.shuffle_code('A', 'Z', 2)
+      @product.code2 = @product.shuffle_code('A', 'Z', 2)
+      @product.code3 = @product.shuffle_code('0', '9', 2)
+
+      if @product.save
+        redirect_to admin_products_path, notice: "商品「#{@product.name}」を登録しました。"
+      else
+        render new_admin_product_path(@product), status: :unprocessable_entity
+      end
     end
 
     def edit
@@ -25,6 +38,14 @@ module Admin
       authenticate_or_request_with_http_basic do |username, password|
         username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
       end
+    end
+
+    def product_params
+      params.require(:product).permit(:name, :description, :min_price, :max_price, :stock, :image)
+    end
+
+    def set_product
+      @product = Product.find(params[:id])
     end
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,15 @@ class Product < ApplicationRecord
   has_one_attached :image
 
   scope :recent, -> { order(created_at: :desc) }
+
+  validates :image, presence: true, blob: { content_type: :image, size_range: 1..5.megabytes }
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :stock, presence: true, numericality: { only_integer: true }
+  validates :min_price, presence: true, numericality: { only_integer: true }
+  validates :max_price, numericality: { only_integer: true, allow_nil: true, greater_than: :min_price }
+
+  def shuffle_code(first_str, last_str, length)
+    (first_str..last_str).to_a.shuffle[0..length].join
+  end
 end
