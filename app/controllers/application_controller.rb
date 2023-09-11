47c51e109e41a96b8d121d0d24d10c3d43cf1698
keita1899 <saboten18k@gmail.com>
2023-09-11 17:54:55 +0900

    current_cartメソッドをprivateに修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@
 class ApplicationController < ActionController::Base
   helper_method :current_cart
 
+  private
+
   def current_cart
     current_cart = Cart.find_by(id: session[:cart_id]) || Cart.create
     session[:cart_id] ||= current_cart.id
